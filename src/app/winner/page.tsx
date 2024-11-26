71d606d5c8005ffc5fb393a8482c3a92a63d22a4
itsmeprinceyt <114930668+itsmeprinceyt@users.noreply.github.com>
2024-11-26 16:57:15 +0530

    fixed /winner

--- src/app/winner/page.tsx
+++ src/app/winner/page.tsx
@@ -17,20 +17,22 @@ const Winner = () => {
     }, [winnerName, router]);
 
     return (
-        <div className="w-screen h-screen bg-gradient-to-b from-green-500 to-green-900 flex justify-center items-center">
-            <div className="top-5 left-5">
-                <HomeButton color="bg-green-800" />
-            </div>
-            <div className="text-center animate-bounce">
-                <h1 className="text-white text-4xl md:text-6xl font-bold mb-6">🥳 The Winner is 🎉</h1>
-                <div className="bg-white text-green-900 text-3xl md:text-5xl lg:text-8xl font-extrabold p-8 rounded-xl shadow-lg">
-                    {winnerName}
+        <>
+            <div className="w-screen h-screen bg-gradient-to-b from-green-500 to-green-900 flex justify-center items-center">
+                <div className="top-5 left-5">
+                    <HomeButton color="bg-green-800" />
+                </div>
+                <div className="text-center animate-bounce">
+                    <h1 className="text-white text-4xl md:text-6xl font-bold mb-6">🥳 The Winner is 🎉</h1>
+                    <div className="bg-white text-green-900 text-3xl md:text-5xl lg:text-8xl font-extrabold p-8 rounded-xl shadow-lg">
+                        {winnerName}
+                    </div>
                 </div>
             </div>
             <div className="bottom-4 w-[250px]">
                 <MadeByMe />
             </div>
-        </div>
+        </>
     );
 };
 
